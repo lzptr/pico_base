152b3f041f3f2dffb9e58ade139afcbc51684e23
lzptr <26966675+lzptr@users.noreply.github.com>
2023-02-28 18:51:34 +0100

    Minor updates to the devcontainer

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ int main() {
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #else
+    stdio_init_all();
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
